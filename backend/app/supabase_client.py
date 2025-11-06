83f03f79ce89a030e0c077e13b463d0d92f90002
robinkov <robinkovacic2@gmail.com>
2025-11-06 17:38:46 +0100

    Check if environment variables are missing

--- backend/app/supabase_client.py
+++ backend/app/supabase_client.py
@@ -7,4 +7,7 @@ load_dotenv()
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
+if SUPABASE_URL is None or SUPABASE_KEY is None:
+  raise Exception("Some environment variables are missing")
+
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
